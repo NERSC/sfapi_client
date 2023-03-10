35799f45513aa0185c29865e0bd0f48cc067857c
Chris Harris <cjh@lbl.gov>
2023-03-10 10:10:33 -0500

    Add download test

--- tests/test_path_async.py
+++ tests/test_path_async.py
@@ -1,19 +1,51 @@
+import pytest
+from pathlib import Path
+
+from sfapi_client import AsyncClient
 from sfapi_client._async.path import RemotePath
 
 
-def test_concat():
-    a = RemotePath("/a")
-    b = RemotePath("b")
-    c = "c"
+@pytest.mark.asyncio
+async def test_download_text(client_id, client_secret, test_machine, test_job_path):
+    async with AsyncClient(client_id, client_secret) as client:
+        machine = await client.compute(test_machine)
+        test_job = Path(test_job_path)
+        test_path = test_job.parent
+        test_name = test_job.name
 
-    new_path = a / b
-    assert isinstance(new_path, RemotePath)
-    assert new_path.name == "b"
-    assert new_path.group is None
-    assert str(new_path) == "/a/b"
+        paths = await machine.listdir(test_path)
 
-    new_path = c / b
-    assert isinstance(new_path, RemotePath)
-    assert new_path.name == "b"
-    assert new_path.group is None
-    assert str(new_path) == "c/b"
+        remote_path = None
+        for p in paths:
+            if p.name == test_name:
+                remote_path = p
+                break
+
+        assert remote_path is not None
+
+        fp = await remote_path.download()
+        contents = fp.read()
+        assert "#SBATCH" in contents
+
+
+@pytest.mark.asyncio
+async def test_download_binary(client_id, client_secret, test_machine, test_job_path):
+    async with AsyncClient(client_id, client_secret) as client:
+        machine = await client.compute(test_machine)
+        test_job = Path(test_job_path)
+        test_path = test_job.parent
+        test_name = test_job.name
+
+        paths = await machine.listdir(test_path)
+
+        remote_path = None
+        for p in paths:
+            if p.name == test_name:
+                remote_path = p
+                break
+
+        assert remote_path is not None
+
+        fp = await remote_path.download(binary=True)
+        bytes = fp.read()
+        assert "#SBATCH" in bytes.decode()
