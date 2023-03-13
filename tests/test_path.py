a90103d7f3f5acb83691bb8569012759a9593c57
Chris Harris <cjh@lbl.gov>
2023-03-13 14:46:39 -0400

    Filter out . and ..

--- tests/test_path.py
+++ tests/test_path.py
@@ -72,3 +72,32 @@ def test_download_binary(client_id, client_secret, test_machine, test_job_path):
         fp = remote_path.download(binary=True)
         bytes = fp.read()
         assert "#SBATCH" in bytes.decode()
+
+
+def test_ls_dir(client_id, client_secret, test_machine, test_job_path, test_username):
+    with Client(client_id, client_secret) as client:
+        machine = client.compute(test_machine)
+        test_job = Path(test_job_path)
+        test_job_directory_name = test_job.parent.name
+        test_job_filename = test_job.name
+        test_job_parent_directory = test_job.parent.parent
+
+        paths = machine.ls(test_job_parent_directory)
+
+        test_job_directory = None
+        for p in paths:
+            if p.name == test_job_directory_name:
+                test_job_directory = p
+                break
+
+        assert test_job_directory is not None
+
+        # Now list the job path an assert the at we can find the test job
+        found = False
+        job_path_entries = test_job_directory.ls()
+        for p in job_path_entries:
+            if p.name == test_job_filename:
+                found = True
+                break
+
+        assert found
