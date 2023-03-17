721ff8c10db825124530ba0be3435a6a57f64558
Chris Harris <cjh@lbl.gov>
2023-03-17 16:35:45 -0400

    Add User model

---
+++ src/sfapi_client/_sync/user.py
@@ -0,0 +1,50 @@
+from typing import List, Optional
+from .._models import (
+    UserInfo as UserBase,
+    ProjectStats as Project,
+    GroupList as GroupsResponse,
+)
+from .group import Group
+from .project import Project
+from .common import SfApiError
+
+
+class User(UserBase):
+    client: Optional["Client"]
+
+    def groups(self) -> List[Group]:
+        if self.name != (self.client._user()).name:
+            raise SfApiError(f"Can only fetch groups for authenticated user.")
+
+        r = self.client.get("account/groups")
+
+        json_response = r.json()
+        groups_reponse = GroupsResponse.parse_obj(json_response)
+
+        groups = [Group.parse_obj(g) for g in groups_reponse.groups]
+
+        def _set_client(g):
+            g.client = self.client
+            return g
+
+        groups = map(_set_client, groups)
+
+        return list(groups)
+
+    def projects(self) -> List[Project]:
+        if self.name != (self.client._user()).name:
+            raise SfApiError(f"Can only fetch projects for authenticated user.")
+
+        r = self.client.get("account/roles")
+
+        json_response = r.json()
+
+        projects = [Project.parse_obj(p) for p in json_response]
+
+        def _set_client(p):
+            p.client = self.client
+            return p
+
+        projects = map(_set_client, projects)
+
+        return list(projects)
