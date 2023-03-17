57d11e4348b460f657d65ad3ea9c3cb4094fc81e
Chris Harris <cjh@lbl.gov>
2023-03-17 16:36:52 -0400

    Add Project Model

---
+++ src/sfapi_client/_async/project.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from pydantic import ValidationError
+
+from .._models import (
+    ProjectStats as ProjectBase,
+)
+from .group import Group
+from .common import SfApiError
+
+
+class Project(ProjectBase):
+    client: Optional["AsyncClient"]
+
+    async def create_group(self, name: str) -> Group:
+        params = {"name": name, "repo_name": self.repo_name}
+
+        r = await self.client.post("account/groups", data=params)
+        json_response = r.json()
+        try:
+            group = Group.parse_obj(json_response)
+        except ValidationError:
+            # See if we have validation error raise it
+            if "details" in json_response:
+                raise SfApiError(r.text())
+            else:
+                raise RuntimeError(r.text())
+
+        return group
