1eafa83b124fcd41e7fcded45bf5328ea786a83f
Chris Harris <cjh@lbl.gov>
2023-03-24 09:38:41 -0400

    Move test configuration in pydantic model

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,48 +1,66 @@
 import os
 import json
+import random
+import string
 
 import pytest
 from authlib.jose import JsonWebKey
 
 from sfapi_client import Machines
 
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    SFAPI_CLIENT_ID: str
+    SFAPI_CLIENT_SECRET: str
+    TEST_JOB_PATH: str
+    TEST_MACHINE: str = Machines.perlmutter
+    TEST_USERNAME: str
+    TEST_ANOTHER_USERNAME: str
+    TEST_TMP_DIR: str
+    TEST_PROJECT: str
+    TEST_GROUP: str
+
+
+    class Config:
+        case_sensitive = True
+        env_file = ".env"
+
+
+settings = Settings()
+
 
 @pytest.fixture
 def client_id():
-    return os.environ["SFAPI_CLIENT_ID"]
+    return settings.SFAPI_CLIENT_ID
 
 
 @pytest.fixture
 def client_secret():
-    json_web_key = os.environ["SFAPI_CLIENT_SECRET"]
+    json_web_key = settings.SFAPI_CLIENT_SECRET
 
     return JsonWebKey.import_key(json.loads(json_web_key))
 
 
 @pytest.fixture
 def test_job_path():
-    return os.environ["TEST_JOB_PATH"]
-
-
-TEST_MACHINE_ENV = "TEST_MACHINE"
+    return settings.TEST_JOB_PATH
 
 
 @pytest.fixture
 def test_machine():
-    if TEST_MACHINE_ENV in os.environ:
-        return Machines[os.environ[TEST_MACHINE_ENV]]
-    else:
-        return Machines.perlmutter
+    return settings.TEST_MACHINE
 
 
 @pytest.fixture
 def test_username():
-    return os.environ["TEST_USERNAME"]
+    return settings.TEST_USERNAME
 
 
 @pytest.fixture
 def test_another_username():
-    return os.environ["TEST_ANOTHER_USERNAME"]
+    return settings.TEST_ANOTHER_USERNAME
 
 
 @pytest.fixture
@@ -52,19 +70,22 @@ def test_file_contents():
 
 @pytest.fixture
 def test_tmp_dir():
-    return os.environ["TEST_TMP_DIR"]
+    return settings.TEST_TMP_DIR
 
 
 @pytest.fixture
 def test_group():
-    return os.environ["TEST_GROUP"]
+    return settings.TEST_GROUP
 
+@pytest.fixture
+def test_random_group():
+    rand =  "".join(
+            random.choices(string.ascii_lowercase + string.digits, k=6)
+        )
+
+    return f"t-{rand}"
 
 @pytest.fixture
 def test_project():
-    return os.environ["TEST_PROJECT"]
+    return settings.TEST_PROJECT
 
-
-@pytest.fixture
-def test_group_create():
-    return os.environ["TEST_GROUP_CREATE"]
