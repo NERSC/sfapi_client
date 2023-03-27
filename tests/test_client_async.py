582a5a085f2cf5de7f9d235af427976b9ca75e24
Chris Harris <cjh@lbl.gov>
2023-03-27 10:48:38 -0400

    Allow client to be created without credentials

---
+++ tests/test_client_async.py
@@ -0,0 +1,18 @@
+import pytest
+import httpx
+
+from sfapi_client import AsyncClient
+
+
+@pytest.mark.asyncio
+async def test_no_creds():
+    async with AsyncClient() as client:
+        assert client is not None
+
+
+@pytest.mark.asyncio
+async def test_no_creds_auth_required(test_machine):
+    async with AsyncClient() as client:
+        machine = await client.compute(test_machine)
+        with pytest.raises(httpx.HTTPStatusError):
+            await machine.jobs()
