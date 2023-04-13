ecab46244e48c1231ec65697bb90602be03870ba
Chris Harris <cjh@lbl.gov>
2023-04-13 14:17:26 -0400

    Reduce wait interval for tests

--- tests/test_jobs_async.py
+++ tests/test_jobs_async.py
@@ -42,7 +42,7 @@ async def test_cancel_wait_for_it(
 
 @pytest.mark.asyncio
 async def test_running(client_id, client_secret, test_job_path, test_machine):
-    async with AsyncClient(client_id, client_secret) as client:
+    async with AsyncClient(client_id, client_secret, wait_interval=1) as client:
         machine = await client.compute(test_machine)
         job = await machine.submit_job(test_job_path)
 
