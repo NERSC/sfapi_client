e9e5c719d78d798c4ac85b3e2c990404f6f3a3fd
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2023-04-27 17:11:05 -0700

    Adding jupyter notebooks

--- docs/gen_examples.py
+++ docs/gen_examples.py
@@ -2,6 +2,37 @@
 from pathlib import Path
 import os
 
+replacements = {
+    "tylern": "elvis",
+    "/global/homes/t": "/global/homes/e",
+    "/global/u1/t": "/global/homes/e",
+    "nstaff": "ntrain",
+    "dasrepo": "ntrain",
+    "95745": "12345"
+}
+
+# Replace personal details in notebook
 for notebook in Path('examples').glob('*.ipynb'):
-    # Hope to replace this with the python library
-    os.system(f'jupyter nbconvert --to markdown {notebook} --output-dir="docs/examples"')
+    # Read in notebook
+    with notebook.open('r') as nb:
+        full_nb = nb.read()
+    # replace all key with value
+    for k, v in replacements.items():
+        full_nb = full_nb.replace(k, v)
+    # Open to write back to notebook
+    with notebook.open('w') as nb:
+        nb.write(full_nb)
+
+# Get our index page
+index_path = Path('docs/examples/index.md')
+
+# Open it up and write heading
+with index_path.open("w") as index:
+    index.write("# Examples \n\n")
+
+    for notebook in Path('examples').glob('*.ipynb'):
+        # Hope to replace this with the python library
+        os.system(f'jupyter nbconvert --to markdown {notebook} --output-dir="docs/examples"')
+
+        notebook_name = " ".join(notebook.stem.title().split("_"))
+        index.write(f"* [{notebook_name}]({notebook.stem})\n")
