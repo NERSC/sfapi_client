537c6913f98f96ade834d2165addd564cd54f645
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2023-05-02 16:52:53 -0700

    Use replacement json for docs gen

---
+++ docs/gen_examples.py
@@ -0,0 +1,38 @@
+
+from pathlib import Path
+import os
+import json
+
+replace = Path("replacements.json")
+
+if replace.exists():
+    with replace.open('r'):
+        replacements = json.loads(replace.read_text())
+else:
+    replacements = {}
+
+# Replace personal details in notebook
+for notebook in Path('examples').glob('*.ipynb'):
+    # Read in notebook
+    with notebook.open('r') as nb:
+        full_nb = nb.read()
+    # replace all key with value
+    for k, v in replacements.items():
+        full_nb = full_nb.replace(k, v)
+    # Open to write back to notebook
+    with notebook.open('w') as nb:
+        nb.write(full_nb)
+
+# Get our index page
+index_path = Path('docs/examples/index.md')
+
+# Ensure the directory exists
+index_path.parent.mkdir(exist_ok=True)
+
+# Open it up and write heading
+with index_path.open("w") as index:
+    index.write("# Examples \n\n")
+
+    for notebook in Path('examples').glob('*.ipynb'):
+        notebook_name = " ".join(notebook.stem.title().split("_"))
+        index.write(f"* [{notebook_name}]({notebook.name})\n")
