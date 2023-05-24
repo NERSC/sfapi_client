59f24c08850be560396b17fc9ea79dd318ca69f1
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2023-05-24 15:11:04 -0700

    Use projects endpoint for projects and add roles

--- src/sfapi_client/_sync/users.py
+++ src/sfapi_client/_sync/users.py
@@ -69,7 +69,7 @@ class User(UserBase):
         if self.name != (self.client._user()).name:
             raise SfApiError(f"Can only fetch projects for authenticated user.")
 
-        r = self.client.get("account/roles")
+        r = self.client.get("account/projects")
 
         json_response = r.json()
 
@@ -82,3 +82,24 @@ class User(UserBase):
         projects = map(_set_client, projects)
 
         return list(projects)
+
+    def roles(self) -> List[Project]:
+        """
+        The roles the user is associate with.
+        """
+        if self.name != (self.client._user()).name:
+            raise SfApiError(f"Can only fetch roles for authenticated user.")
+
+        r = self.client.get("account/roles")
+
+        json_response = r.json()
+
+        roles = [Project.parse_obj(p) for p in json_response]
+
+        def _set_client(p):
+            p.client = self.client
+            return p
+
+        roles = map(_set_client, roles)
+
+        return list(roles)
