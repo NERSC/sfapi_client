2dcaae12b273cb37d5827393bc5f9d4c5f8e53b9
MrCreosote <MrCreosote@users.noreply.github.com>
2025-01-24 12:01:35 -0800

    Import `OutageStatusValue` in `__init__.py`
    Handle imports for OutageStatusValue in the same way as StatusValue.
    
    I tested this locally, seems to work

--- src/sfapi_client/__init__.py
+++ src/sfapi_client/__init__.py
@@ -1,5 +1,6 @@
 from .client import AsyncClient  # noqa: F401
 from .client import Client  # noqa: F401
 from .exceptions import SfApiError  # noqa: F401
+from ._models import OutageStatusValue  # noqa: F401
 from ._models import StatusValue  # noqa: F401
 from ._models.resources import Resource  # noqa: F401
